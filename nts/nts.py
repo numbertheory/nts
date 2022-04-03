cb6b0b935bea51d00e2ba3793298d51d6ae1271a
JP Etcheber <jetcheber@gmail.com>
2022-04-03 10:57:22 -0700

    using poetry for dependenices

--- nts/nts.py
+++ nts/nts.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python3
 
 from nts.argument_parse import collect
+import toml
 import os
 
 def config(config_file):
-    return collect.Arguments(os.path.dirname(__file__) + '/../' + config_file)
+    config_file_path = collect.Arguments(os.path.dirname(__file__) + '/../' + config_file)
