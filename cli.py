8e578c2edd9e7568b9c0d7ff4ea4cdb118c093f5
JP Etcheber <jetcheber@gmail.com>
2022-04-03 12:19:20 -0700

    support for setting up path to config

--- cli.py
+++ cli.py
@@ -1,7 +1,14 @@
 from nts import nts
+import os
 
 
-config = nts.Config("arguments.yaml")
+args = nts.Config("arguments.yaml")
 
-print(config.file_path)
-print(config.values())
+def run_cli():
+    config_set = nts.check_for_configuration(args)
+    if config_set.get(args.journal):
+        return(config_set.get(args.journal))
+
+
+if __name__ == "__main__":
+    print(run_cli())
--- nts/nts.py
+++ nts/nts.py
@@ -5,9 +5,29 @@ import toml
 import os
 
 class Config:
-    def __init__(self, config_file):
-        self.args_file_path = os.path.dirname(__file__) + '/../' + config_file
-        self.file_path = collect.Arguments(self.args_file_path).value("config")
+    def __init__(self, args_file):
+        self.args = os.path.dirname(__file__) + '/../' + args_file
+        self.file_path = os.path.expanduser(collect.Arguments(self.args).value("config"))
+        self.journal = collect.Arguments(self.args).value("journal")
 
     def values(self):
-        return toml.load(os.path.expanduser(self.file_path))
+        return toml.load(self.file_path)
+
+
+def check_for_configuration(config):
+    if os.path.isfile(config.file_path):
+        return config.values()
+    else:
+        default_journal_path = os.path.expanduser("~/.local/share/nts/default/journal.json")
+        default_toml = {"default": {"journal_path": "{}".format(default_journal_path)}}
+        user_set_path = input("Set path for config: [{}] ?".format(config.file_path))
+        if not user_set_path:
+            user_set_path = config.file_path
+        try:
+            os.makedirs(os.path.dirname(user_set_path))
+        except FileExistsError:
+            pass
+        with open(user_set_path, "w") as config_file:
+            config_file.write(toml.dumps(default_toml))
+            print("Configuration created.")
+        return Config("arguments.yaml").values()
