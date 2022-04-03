fa977a783539d926a9159902b6c30a986d96a94e
JP Etcheber <jetcheber@gmail.com>
2022-04-03 12:44:09 -0700

    error on no journal found

--- cli.py
+++ cli.py
@@ -7,8 +7,12 @@ args = nts.Config("arguments.yaml")
 def run_cli():
     config_set = nts.check_for_configuration(args)
     if config_set.get(args.journal):
-        return(config_set.get(args.journal))
+        return [config_set.get(args.journal), 0]
+    else:
+        return ["Journal not found: {}".format(args.journal), 1]
 
 
 if __name__ == "__main__":
-    print(run_cli())
+    command = run_cli()
+    print(command[0])
+    exit(command[1])
