4c7f9aeda483482e4109c83d8ff9ec7400ec50ad
JP Etcheber <jetcheber@gmail.com>
2022-04-03 12:58:05 -0700

    check for journal

--- cli.py
+++ cli.py
@@ -7,7 +7,9 @@ args = nts.Config("arguments.yaml")
 def run_cli():
     config_set = nts.check_for_configuration(args)
     if config_set.get(args.journal):
-        return [config_set.get(args.journal), 0]
+        if args.debug:
+            print(config_set.get(args.journal))
+        return [nts.check_for_journal(config_set.get(args.journal)), 0]
     else:
         return ["Journal not found: {}".format(args.journal), 1]
 
